21896bf1f10f031a73739abad1f3a66621577d30
Jorge-Peralta <jorge9890@hotmail.com>
2022-05-23 00:08:38 -0600

    edit

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const router = express.Router();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -15,12 +13,12 @@ app.listen(3000, () => console.log("App listening on port 3000"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "public", "artistas.html"));
 });
 
 app.use("/api", canciones);
 
 app.get("*", (req, res) => {
-  res.status(404).send("<h1> Whoops! Page not found</h1>");
+  res.status(404).send("<h1> Ojo un error</h1>");
 });
